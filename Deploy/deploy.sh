0f4ec96b05e86dc1901719d46e910e7458fca8ec
ewerton255 <e.d.correia@hotmail.com>
2021-11-28 11:45:03 -0300

    Enhancement on application.properties

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -1,30 +1,62 @@
 #!/bin/bash
 
 clear
-echo 'Starting deploy...'
-echo 'Compiling application...'
+
+echo " -----------------------------------------------------------------------"
+echo '| Project: Person serive                                                |'
+echo '| Repository: https://github.com/ewerton255/person_service              |'
+echo '|               © 2021 Edcor. All Rights Reserved.                      |'
+echo " -----------------------------------------------------------------------"
+
+sleep 1s
+
+echo " -----------------------------------------------------------------------"
+echo '|                         Starting deploy                               |'
+echo " -----------------------------------------------------------------------"
+
+sleep 1s
+
+echo " -----------------------------------------------------------------------"
+echo '|                  Starting compiling application...                    |'
+echo " -----------------------------------------------------------------------"
 
 cd ../Sources/PersonApi
 
+sleep 2s
+
 mvn clean package -X -DskipTests
 
-echo 'Copying generated .jar...'
+sleep 1s
+
+echo " -----------------------------------------------------------------------"
+echo '|                        Copying generated .jar...                      |'
+echo " -----------------------------------------------------------------------"
 
 cd ../../Deploy
 
 cp ../Sources/PersonApi/target/*.jar ./app.jar
 
-echo 'Starting deploy with docker-comopose in background mode...'
+sleep 1s
+
+echo " -----------------------------------------------------------------------"
+echo '|      Starting deploy with docker-comopose in background mode...       |'
+echo " -----------------------------------------------------------------------"
 
 docker-compose up -d --build
 
-echo 'Removing .jar...'
+sleep 1s
+
+echo " -----------------------------------------------------------------------"
+echo '|                          Removing .jar...                             |'
+echo " -----------------------------------------------------------------------"
 
 rm -f ./app.jar
 
 sleep 1s
 
-echo 'Deploy successful!'
+echo " -----------------------------------------------------------------------"
+echo '|                       Deploy successful!                              |'
+echo " -----------------------------------------------------------------------"
 
 sleep 1s
 
